fb7828d671050698e7c130733d7e5dd9ebc8e68d
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-04 15:35:46 +0600

    67-2 Use Dot Env To Hide Credentials

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 require('colors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //* Middleware
 app.use(cors());
 app.use(express.json());
+
+// Mongodb Atlas
+// username: geniusDBUser
+// password: hAUHLBYUur9OGuvi
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected'.yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+
+dbConnect();
 
 app.get('/', (req, res) => {
   res.send('Genius car Server Side');
