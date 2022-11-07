642cdf90c4ab22b686da9392ec5131ae8d5b561b
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-07 13:11:51 +0600

    deploy to vercel

---
+++ __deploy.steps.js
@@ -0,0 +1,10 @@
+/* 
+ //* https://fahimahammed-cse.medium.com/deploy-an-express-api-on-vercel-eebc13ace629
+
+
+1. add vercel.config file
+2. vercel
+3. add: "build": "node index.js", in your package.json file
+
+
+*/
